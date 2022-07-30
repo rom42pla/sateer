c16663760a6c0327fc5773b1517e190aa39e7259
rom42pla <romeo.lanzino@gmail.com>
2022-07-30 17:50:33 +0200

    updated architecture

--- models/feegt.py
+++ models/feegt.py
@@ -136,7 +136,10 @@ class FouriEEGTransformer(pl.LightningModule):
         #     Rearrange("b s c m -> b s (c m)"),
         # )
         self.merge_mels = nn.Sequential(
-            nn.Linear(in_features=128//2 + 1, out_features=1),
+            nn.Linear(in_features=128//2 + 1, out_features=self.window_embedding_dim),
+            nn.SELU(),
+            nn.Linear(in_features=self.window_embedding_dim, out_features=1),
+            nn.AlphaDropout(self.dropout_p),
             Rearrange("b s c m -> b s (c m)"),
             FouriEncoderBlock(in_features=self.in_channels,
                               mid_features=self.window_embedding_dim * 4,
