93af172d1033ea640cffe64b4c614495e5f5f145
rom42pla <romeo.lanzino@gmail.com>
2022-08-13 18:59:22 +0200

    added condor train

--- condor_train.sh
+++ condor_train.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+if [ ! -d $(pwd)/condor_logs ]
+then
+    mkdir $(pwd)/condor_logs
+    echo "created $(pwd)/condor_logs"
+fi
+
 printf "universe = docker
 docker_image = visionlabsapienza/workgroup:container-08082022
 executable = /bin/python3
-arguments = /data1/visionlab/train.py $*
-output = ./condor_logs/out_\$(ClusterId)
-error = ./condor_logs/err_\$(ClusterId)
-log = ./condor_logs/err_\$(ClusterId)
+arguments = $(pwd)/train.py $*
+output = $(pwd)/condor_logs/out_\$(ClusterId)
+error = $(pwd)/condor_logs/err_\$(ClusterId)
+log = $(pwd)/condor_logs/log_\$(ClusterId)
 request_cpus = 1
 request_gpus = 1
 request_memory = 64G
@@ -14,22 +20,7 @@ request_disk = 100G
 +MountData2=FALSE
 +MountHomes=FALSE
 queue 1" > run.sub
-#echo 'universe = docker' > $project_path/run.sub
-#echo 'docker_image = visionlabsapienza/workgroup:container-08082022' >> $project_path/run.sub
-#echo 'executable = /bin/python3' >> $project_path/run.sub
-#echo 'arguments = /data1/visionlab/train.py ' $arguments >> $project_path/run.sub
-#echo 'output = ./CondorLogs/out.$(ClusterId).$(ProcId)' >> $project_path/run.sub
-#echo 'error = ./CondorLogs/err.$(ClusterId).$(ProcId)' >> $project_path/run.sub
-#echo 'log = ./CondorLogs/log.$(ClusterId).$(ProcId)' >> $project_path/run.sub
-#echo 'request_cpus = 1' >> $project_path/run.sub
-#echo 'request_gpus = 1' >> $project_path/run.sub
-#echo 'request_memory = 64G' >> $project_path/run.sub
-#echo 'request_disk = 100G' >> $project_path/run.sub
-#echo '+MountData1=TRUE' >> $project_path/run.sub
-#echo '+MountData2=FALSE' >> $project_path/run.sub
-#echo '+MountHomes=FALSE' >> $project_path/run.sub
-#echo 'queue 1' >> $project_path/run.sub
-#
+
 condor_submit run.sub
 rm run.sub
 condor_q
