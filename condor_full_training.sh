21f4ac857d21e77312800283ec66e9e788aa6f27
rom42pla <romeo.lanzino@gmail.com>
2022-08-20 09:39:56 +0200

    determined best parameters for training

--- condor_full_training.sh
+++ condor_full_training.sh
@@ -2,8 +2,8 @@ script_starter="bash cluster_train.sh"
 shared_params="--checkpoints_path=./checkpoints --seed=42"
 
 deap_command="$script_starter deap data/deap --spectrogram_time_masking_perc=0 --spectrogram_frequency_masking_perc=0 --disable_flipping $shared_params"
-$deap_command --batch_size=2048 --windows_size=1 --windows_stride=1
-$deap_command --batch_size=2048 --windows_size=2 --windows_stride=1
+$deap_command --batch_size=1024 --windows_size=1 --windows_stride=1
+$deap_command --batch_size=1024 --windows_size=2 --windows_stride=1
 $deap_command --batch_size=256 --windows_size=10 --windows_stride=2
 
 dreamer_command="$script_starter dreamer data/dreamer --spectrogram_time_masking_perc=0 --spectrogram_frequency_masking_perc=0 --mel_window_size=1 --mel_window_stride=0.05 --disable_flipping $shared_params"
@@ -17,6 +17,6 @@ $seed_command --batch_size=1024 --windows_size=1 --windows_stride=1
 $seed_command --batch_size=1024 --windows_size=2 --windows_stride=2
 
 amigos_command="$script_starter amigos data/amigos --num_decoders=4 --spectrogram_time_masking_perc=0 --spectrogram_frequency_masking_perc=0 --disable_flipping $shared_params"
-$amigos_command --batch_size=2048 --windows_size=1 --windows_stride=1
-$amigos_command --batch_size=2048 --windows_size=2 --windows_stride=2
+$amigos_command --batch_size=1024 --windows_size=1 --windows_stride=1
+$amigos_command --batch_size=1024 --windows_size=2 --windows_stride=2
 
